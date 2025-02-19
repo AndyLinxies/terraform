7eec9e7bd9fb96224ad43afcc407b97c21d9ed68
AndyLinxies <linxies@gmail.com>
2025-02-19 22:11:20 +0100

    instance avec ports ouverts et certs

---
+++ main.tf
@@ -0,0 +1,52 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "random" {}
+
+resource "random_pet" "name" {}
+
+resource "aws_instance" "web" {
+  ami           = "ami-a0cfeed8"
+  instance_type = "t2.micro"
+  user_data     = file("init-script.sh")
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  key_name      = "cert" 
+  tags = {
+    Name = random_pet.name.id
+  }
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.name.id}-sg"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
+#   from_port         = 443
+#   ip_protocol       = "tcp"
+#   to_port           = 443
+# }
